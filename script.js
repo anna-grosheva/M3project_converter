10d51dc33c25ce4a3ea2efab65b4c22b38cfc872
Anna Grosheva <ap.grosheva@gmail.com>
2020-08-26 21:53:37 +0400

    26/08 latest upd

--- script.js
+++ script.js
@@ -10,16 +10,23 @@ let currencyButtonsTo = document.querySelectorAll('.to .currency__buttons li');
 let selectFrom = document.querySelector('.from select');
 let selectTo = document.querySelector('.to select');
 let loading = document.querySelector('.loading');
+const regexp = /^[0-9\.]+$/;
 
 
-//проверка на символ при вводе
+//проверка на символ при вводе и запросы к серверу с фокуса
 inputValues.forEach((inputValue) => {
     inputValue.addEventListener('keyup', (event) => {
-        if (!(event.keyCode >= 48 && event.keyCode <= 57) && !(event.keyCode >= 96 && event.keyCode <= 105) && (event.keyCode !== 188) && (event.keyCode !== 190) && (event.keyCode !== 8)) {
+        inputValue.value = inputValue.value.replace(',', '.');
+        if (!regexp.test(inputValue.value)) {
             inputValue.value = inputValue.value.replace(inputValue.value[inputValue.value.length - 1], '');
-        } else if (event.keyCode === 188) {
-            inputValue.value = inputValue.value.replace(',', '.');
         } 
+    });
+    inputValue.addEventListener('focus', (event) => {
+        if (currencyButtonsFrom[currencyButtonsFrom.length - 1].classList.contains('currency__active') && currencyButtonsTo[currencyButtonsFrom.length - 1].classList.contains('currency__active')) {
+            enquiry('change');
+        } else {
+            enquiry('click');
+        }
     });
 });
 
@@ -52,8 +59,8 @@ function sameCurrencies() {
 
 
 // переключение кнопок валют
-currencyButtonsFrom.forEach((currencyButton,index) => {
-    if (index < 4) {
+currencyButtonsFrom.forEach((currencyButton, index) => {
+    if (index < (currencyButtonsFrom.length - 1)) {
         currencyButtonsFrom[index].addEventListener('click', () => {
             activeButtonFrom(index);
             if (currActiveFrom.innerText !== currActiveTo.innerText) {
@@ -65,7 +72,7 @@ currencyButtonsFrom.forEach((currencyButton,index) => {
     }  
 });
 currencyButtonsTo.forEach((currencyButton, index) => {
-    if (index < 4) {
+    if (index < (currencyButtonsTo.length - 1)) {
         currencyButtonsTo[index].addEventListener('click', () => {
             activeButtonTo(index);
             if (currActiveTo.innerText !== currActiveFrom.innerText) {
@@ -76,12 +83,12 @@ currencyButtonsTo.forEach((currencyButton, index) => {
         });
     }
 });
-currencyButtonsFrom[4].addEventListener('change', () => {
-    activeButtonFrom(4);
+currencyButtonsFrom[currencyButtonsFrom.length - 1].addEventListener('change', () => {
+    activeButtonFrom(currencyButtonsFrom.length - 1);
     enquiry(event.type);
 });
-currencyButtonsTo[4].addEventListener('change', () => {
-    activeButtonTo(4);
+currencyButtonsTo[currencyButtonsTo.length - 1].addEventListener('change', () => {
+    activeButtonTo(currencyButtonsTo.length - 1);
     enquiry(event.type);
 });
 
@@ -91,31 +98,31 @@ let interchangeButton = document.querySelector('.interchange');
 interchangeButton.addEventListener('click', () => {
     let currActiveTo = document.querySelector('.to .currency__active');
     let currActiveFrom = document.querySelector('.from .currency__active');
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
+    for (let i = 0; i < (currencyButtonsFrom.length - 1); i++) {
+        for (let j = 0; j < (currencyButtonsFrom.length - 1); j++) {
             if (currencyButtonsFrom[i].innerText === currActiveTo.innerText) {
                 if (currencyButtonsTo[j].innerText === currActiveFrom.innerText) {
                     activeButtonFrom(i);
                     activeButtonTo(j);
                     enquiry(event.type);
-                }  else if (currencyButtonsFrom[4].classList.contains('currency__active')) {
+                }  else if (currencyButtonsFrom[currencyButtonsFrom.length - 1].classList.contains('currency__active')) {
                     selectTo.value = selectFrom.value;
-                    activeButtonTo(4);
+                    activeButtonTo(currencyButtonsFrom.length - 1);
                     activeButtonFrom(i);
                     enquiry(event.type);
                 }    
             }
-            if (currencyButtonsTo[4].classList.contains('currency__active')) {
+            if (currencyButtonsTo[currencyButtonsFrom.length - 1].classList.contains('currency__active')) {
                 if (currencyButtonsTo[j].innerText === currActiveFrom.innerText) {
                     selectFrom.value = selectTo.value;
-                    activeButtonFrom(4);
+                    activeButtonFrom(currencyButtonsFrom.length - 1);
                     activeButtonTo(j);
                     enquiry(event.type);
                 }
             }
         }
     }
-    if (currencyButtonsFrom[4].classList.contains('currency__active') && currencyButtonsTo[4].classList.contains('currency__active')) {
+    if (currencyButtonsFrom[currencyButtonsFrom.length - 1].classList.contains('currency__active') && currencyButtonsTo[currencyButtonsFrom.length - 1].classList.contains('currency__active')) {
         let temp = selectFrom.value;
         selectFrom.value = selectTo.value;
         selectTo.value = temp;
@@ -127,18 +134,18 @@ interchangeButton.addEventListener('click', () => {
 // функция запроса к серверу
 function enquiry(param) {
     let A, B;
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (currencyButtonsFrom[i].classList.contains('currency__active') && currencyButtonsTo[j].classList.contains('currency__active') && param == 'click') {
+    for (let i = 0; i < currencyButtonsFrom.length - 1; i++) {
+        for (let j = 0; j < currencyButtonsFrom.length - 1; j++) {
+            if (currencyButtonsFrom[i].classList.contains('currency__active') && currencyButtonsTo[j].classList.contains('currency__active') && (param == 'click')) {
                 A = currActiveFrom.innerText;
                 B = currActiveTo.innerText;
-            } else if (currencyButtonsFrom[4].classList.contains('currency__active') && currencyButtonsTo[j].classList.contains('currency__active') && (param == 'change' || param == 'click')) {
+            } else if (currencyButtonsFrom[currencyButtonsFrom.length - 1].classList.contains('currency__active') && currencyButtonsTo[j].classList.contains('currency__active') && (param == 'change' || param == 'click')) {
                 A = selectFrom.value;
                 B = currActiveTo.innerText;
-            } else if (currencyButtonsFrom[i].classList.contains('currency__active') && currencyButtonsTo[4].classList.contains('currency__active') && (param == 'change' || param == 'click')) {
+            } else if (currencyButtonsFrom[i].classList.contains('currency__active') && currencyButtonsTo[currencyButtonsFrom.length - 1].classList.contains('currency__active') && (param == 'change' || param == 'click')) {
                 A = currActiveFrom.innerText;
                 B = selectTo.value;
-            } else if (currencyButtonsFrom[4].classList.contains('currency__active') && currencyButtonsTo[4].classList.contains('currency__active') && param == 'change') {
+            } else if (currencyButtonsFrom[currencyButtonsFrom.length - 1].classList.contains('currency__active') && currencyButtonsTo[currencyButtonsFrom.length - 1].classList.contains('currency__active') && param == 'change') {
                 A = selectFrom.value;
                 B = selectTo.value;
             }
@@ -150,17 +157,16 @@ function enquiry(param) {
     let waiting = setTimeout(() => {
         loading.style.display = 'block';
     }, 500)
-    
-    setTimeout(() => {
+
+    function hideLoading() {
         loading.style.display = 'none';
         clearTimeout(waiting);
-    }, 5000)
+    }
 
     getPromise
     .then(response => response.json())
     .then(data => {
-        loading.style.display = 'none';
-        clearTimeout(waiting);
+        hideLoading();
         inputValues.forEach((inputValue) => {
             exRateFrom.innerText = `1 ${A} = ${+(1 / data.rates[A]).toFixed(4)} ${B}`;
             exRateTo.innerText = `1 ${B} = ${+data.rates[A].toFixed(4)} ${A}`;
@@ -168,19 +174,20 @@ function enquiry(param) {
             inputValueTo.value = +(inputValueFrom.value / data.rates[A]).toFixed(4);
 
             inputValueFrom.addEventListener('input', () => {
-                if (inputValueFrom.value > 0) {
+                if (inputValueFrom.value >= 0) {
                     inputValueTo.value = +(inputValueFrom.value / data.rates[A]).toFixed(4);
-                } else {
-                    inputValueTo.value = '';
                 }   
             });
             inputValueTo.addEventListener('input', () => {
-                inputValueFrom.value = +(inputValueTo.value * data.rates[A]).toFixed(4);
+                if (inputValueTo.value >= 0) {
+                    inputValueFrom.value = +(inputValueTo.value * data.rates[A]).toFixed(4);
+                }
             });
         });  
     })
     .catch((error) => {
         alert('Ошибка сервера или нет интернет-соединения');
+        hideLoading();
     }); 
 }
 enquiry('click');
